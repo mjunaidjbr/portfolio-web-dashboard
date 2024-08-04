9ecd9e66ca4f0f5df292a9b2d1fb9dddaf6cb6e6
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-08-04 13:03:25 +0500

    Add settings structure and environment variables

--- portfolioLedger/settings/base.py
+++ portfolioLedger/settings/base.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pi4*l8030q%gbvj@$a+uep@o_@!+s11+v9qewu7#f52#$7117%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DEBUG') == 'True'
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
