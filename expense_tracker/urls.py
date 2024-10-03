220828e2e41559dbc0815fc78349d00d931a9703
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-03 16:33:43 +0500

    delete_payment_method url added

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('',views.expense_tracker_dashboard, name="expense_dashboard"),
     path('login/',views.login_page, name="login_page"),
     path('logout/',views.logout_user, name="logout_page"),
+    path('delete-payment-method/', views.delete_payment_method, name='delete_payment_method'),
 
 ]
