aef71fb73e5e1d2c21ce27e3b9aaf2d4f7b04c45
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-03 11:30:51 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',  // Django templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
