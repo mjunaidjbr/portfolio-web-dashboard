d8e6202fce5cc7895658f0b6cc90935642695d6a
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-03 21:17:45 +0500

    Update forms.py

--- expense_tracker/forms.py
+++ expense_tracker/forms.py
@@ -5,3 +5,12 @@ class PaymentMethodForm(forms.ModelForm):
     class Meta:
         model = PaymentMethod
         exclude = ['user', 'created_at', 'updated_at']  # Exclude user, created_at, and updated_at fields
+
+
+from django import forms
+from .models import FundsTransaction
+
+class FundsTransactionForm(forms.ModelForm):
+    class Meta:
+        model = FundsTransaction
+        exclude = ['payment_method', 'created_at', 'updated_at']  # Exclude these fields
