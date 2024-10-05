ee7c5c333635f679a9401f34551e552918933791
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-05 22:48:33 +0500

    ExpenseForm added

--- expense_tracker/forms.py
+++ expense_tracker/forms.py
@@ -14,3 +14,12 @@ class FundsTransactionForm(forms.ModelForm):
     class Meta:
         model = FundsTransaction
         exclude = ['payment_method', 'created_at', 'updated_at']  # Exclude these fields
+
+
+from .models import Expense
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        exclude = ['user', 'created_at', 'updated_at', 'payment_method']
+    
