526d138f60e239f4d7a10ca26ac94904bfba6654
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-08 04:02:58 +0500

    Update urls.py

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('login/',views.login_page, name="login_page"),
     path('logout/',views.logout_user, name="logout_page"),
     path('delete-payment-method/', views.delete_payment_method, name='delete_payment_method'),
+    path('filter-expenses/', views.filter_expenses, name='filter_expenses'),
+    path('search-expenses/', views.search_expenses, name='search_expenses'),
+    path('delete-expense/<int:expense_id>/', views.delete_expense, name='delete_expense'),
+    
 
 ]
