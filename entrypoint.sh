32bc2e44dd93cee40cda46184454afc417dfc283
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-14 04:47:57 +0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Wait for the database to be ready
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+# Run makemigrations and migrate
+python manage.py makemigrations
+python manage.py migrate
+
+# Start the Gunicorn server
+exec gunicorn --bind 0.0.0.0:8000 portfolioLedger.wsgi:application
