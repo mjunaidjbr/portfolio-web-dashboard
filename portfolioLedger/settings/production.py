eb353fc945b32928905c1e8dfe9ad910c72f72f7
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-14 18:15:49 +0500

    Update production.py

--- portfolioLedger/settings/production.py
+++ portfolioLedger/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 # ROOT_URLCONF = 'portfolioLedger.urls'
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
