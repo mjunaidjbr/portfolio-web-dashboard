842c68ab03b389c1b17897fecbf4e94b62c85297
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-14 17:36:25 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ done
 # Run makemigrations and migrate
 python manage.py makemigrations
 python manage.py migrate
+# python manage.py collectstatic --noinput
 
 # Start the Gunicorn server
 exec gunicorn --bind 0.0.0.0:8000 portfolioLedger.wsgi:application
